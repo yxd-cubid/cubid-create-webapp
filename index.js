a1bf026db924a92b0f01932f6015c405198d7b87
cubid <yxd.cubid@gmail.com>
2021-01-24 20:38:50 +0800

    添加create 指令

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+const program = require("commander");
+
+const createCommands = require("./lib/core/createCommands");
+
+// 写入版本信息
+program.version(require("./package.json").version);
+// 创建命令
+createCommands();
+// 启动 commander
+program.parse(process.argv);
